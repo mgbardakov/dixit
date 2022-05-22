60e16d2b9772385e4cddc71a9cc2daa49f0d377d
mgbardakov <mgbardakov@gmail.com>
2022-05-22 13:48:43 +0600

    Unit testing lobby event service

--- src/test/java/site/hornsandhooves/dixit/TestDataFactory.java
+++ src/test/java/site/hornsandhooves/dixit/TestDataFactory.java
@@ -3,6 +3,9 @@ package site.hornsandhooves.dixit;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.support.GenericMessage;
+import site.hornsandhooves.dixit.model.events.lobby.LobbyEvent;
+import site.hornsandhooves.dixit.model.events.lobby.LobbyEventType;
+import site.hornsandhooves.dixit.model.lobby.Lobby;
 import site.hornsandhooves.dixit.model.lobby.User;
 
 import java.security.Principal;
@@ -11,23 +14,35 @@ import java.util.UUID;
 
 public class TestDataFactory {
 
-    private static final String user1Id = "f12184fd-2071-48b6-861c-659bef1a4385";
-    private static final String subscriptionId = "sub-0";
+    private static final String USER_1_ID = "f12184fd-2071-48b6-861c-659bef1a4385";
+    private static final String SUBSCRIPTION_ID = "sub-0";
+    private static final String LOBBY_ID = "abc123";
 
     public static User getUser() {
-        return User.builder().id(UUID.fromString(user1Id)).name("Biba").host(true).build();
+        return User.builder().id(UUID.fromString(USER_1_ID)).name("Biba").host(true).build();
     }
 
     public static Message<?> getSubscribeMessage() {
         Map<String, Object> headers = Map.of("simpMessageType", SimpMessageType.SUBSCRIBE,
-                "simpDestination", "Expected destination", "simpUser", (Principal) () -> user1Id,
-                "simpSubscriptionId", subscriptionId);
+                "simpDestination", "Expected destination", "simpUser", (Principal) () -> USER_1_ID,
+                "simpSubscriptionId", SUBSCRIPTION_ID);
         return new GenericMessage<Object>(new byte[0], headers);
     }
 
     public static Message<?> getUnsubscribeMessage() {
         Map<String, Object> headers = Map.of("simpMessageType", SimpMessageType.UNSUBSCRIBE,
-                "simpUser", (Principal) () -> user1Id, "simpSubscriptionId", subscriptionId);
+                "simpUser", (Principal) () -> USER_1_ID, "simpSubscriptionId", SUBSCRIPTION_ID);
         return new GenericMessage<Object>(new byte[0], headers);
     }
+
+    public static LobbyEvent getCreateLobbyEvent() {
+        return new LobbyEvent(LobbyEventType.CREATE_LOBBY, null);
+    }
+
+    public static Lobby getLobby() {
+        var lobby = new Lobby();
+        lobby.setLobbyId(LOBBY_ID);
+        return lobby;
+    }
+
 }
