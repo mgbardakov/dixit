9fa0dcd33eb7d2f45a663695ccc4afa56a4ce07a
mgbardakov <mgbardakov@gmail.com>
2022-05-24 15:05:50 +0600

    fixing cors

--- src/main/java/site/hornsandhooves/dixit/config/WebSocketConfig.java
+++ src/main/java/site/hornsandhooves/dixit/config/WebSocketConfig.java
@@ -1,8 +1,11 @@
 package site.hornsandhooves.dixit.config;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -25,8 +28,19 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gs-guide-websocket")
-                .setHandshakeHandler(new InmemoryUserHandshakeHandler(userService))
                 .setAllowedOrigins("*")
+                .setHandshakeHandler(new InmemoryUserHandshakeHandler(userService))
                 .withSockJS();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer () {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*")
+                        .allowedOriginPatterns("*");
+            }
+        };
     }
 }
